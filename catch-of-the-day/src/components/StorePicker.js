d8dfe399fb8b803dd27a0cb6ecdcbd2c913914b6
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-06-29 15:35:19 -0400

    Broke out StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>Did the second one work? It did.</p>
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
 
-class StorePicker extends React.Component {
-    render() {
-        return <p>This is the Store Picker Component</p>
-    }
-}
 
 render(<StorePicker />, document.querySelector('#main'));
