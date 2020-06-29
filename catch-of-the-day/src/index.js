edcd182a70fcd826a2df77f6406fc57df196cb5c
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-06-29 15:18:23 -0400

    Setup component, render on index.html

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>Hi!</p>
+    }
+}
+
+render(<p>This is redeered on the page</p>, document.querySelector('#main'));
